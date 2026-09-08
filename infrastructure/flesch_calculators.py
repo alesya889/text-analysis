6080a04ec9ea72f40557f74bb7273ca5ae26a69f
valery <iva.sim2007@gmail.com>
2026-09-08 19:20:55 +0700

    formule of flesh_kincaid

--- infrastructure/flesch_calculators.py
+++ infrastructure/flesch_calculators.py
@@ -19,3 +19,20 @@ def flesch_index(stats: TextStats, lang: Languages_Used) -> float:
     elif lang == Languages_Used.FRANCE:
         return 206.835 - 1.015 * stats.avg_sentence_length - 84.6 * stats.avg_word_syllables
 
+
+def flesch_kincaid(stats: TextStats, lang: Languages_Used) -> float:
+    """Calculate Flesch's Kincaid Index"""
+
+    if lang == Languages_Used.ENGLISH:
+        return 0.39 * stats.avg_sentence_length + 11.8 * stats.avg_word_syllables - 15.59
+
+    elif lang == Languages_Used.RUSSIAN:
+        return 206.863 - (1.52 * stats.avg_sentence_length) - (65.14 * stats.avg_word_syllables)
+
+    elif lang == Languages_Used.GERMAN:
+        return 0.39 * stats.avg_sentence_length + 11.8 * stats.avg_word_syllables - 15.59
+
+    elif lang == Languages_Used.FRANCE:
+        return 0.39 * stats.avg_sentence_length + 11.8 * stats.avg_word_syllables - 15.59
+
+
