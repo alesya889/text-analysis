461a77acee9b6c070e48c9facb6a4f6b3267c5d8
valery <iva.sim2007@gmail.com>
2026-09-08 20:31:25 +0700

    sentiment is done

---
+++ infrastructure/sentiment.py
@@ -0,0 +1,32 @@
+from domain.types import Polarity, Languages_Used
+from textblob import TextBlob
+from infrastructure.language_detector import detect_language
+
+
+def analyze_sentiment(text: str) -> tuple[Polarity, float]:
+    lang = detect_language(text)
+
+    blob = TextBlob(text)
+    polarity = blob.sentiment.polarity
+    subjectivity = blob.sentiment.subjectivity
+
+    if lang != Languages_Used.ENGLISH:
+        try:
+            translated = blob.translate(to='en')
+            if translated and str(translated).strip():
+                blob = TextBlob(str(translated))
+                polarity = blob.sentiment.polarity
+                subjectivity = blob.sentiment.subjectivity
+        except:
+            pass
+
+    if polarity > 0.1:
+        p = Polarity.Positive
+
+    elif polarity < -0.1:
+        p = Polarity.Negative
+
+    else:
+        p = Polarity.Neutral
+
+    return p, subjectivity
