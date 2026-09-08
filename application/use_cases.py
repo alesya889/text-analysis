7a396479de1ec7975a89e918b107acad10fb1d99
valery <iva.sim2007@gmail.com>
2026-09-08 12:27:14 +0700

    interpretation of flesh in use_cases

--- application/use_cases.py
+++ application/use_cases.py
@@ -1,6 +1,7 @@
-from domain.types import TextStats, AnalysisResult, Languages_Used, Polarity
+from domain.types import TextStats, AnalysisResult, Languages_Used, Polarity, Difficulty_Level
 from domain.interfaces import SyllableCounter, SentimentAnalyzer
 import re
+import infrastructure.flesch_calculators
 
 def split_sentences(text: str) -> list[str]:
     import re
@@ -27,3 +28,49 @@ def compute_stats(text: str, syllable_counter: SyllableCounter) -> TextStats:
         avg_sentence_length=avg_sentence_length,
         avg_word_syllables=avg_word_syllables
     )
+
+def interpret_flesch(score: float, lang: Languages_Used) -> str:
+    if lang == Languages_Used.ENGLISH:
+        if 100 >= score >= 90:
+            return Difficulty_Level.Very_Easy.value
+        elif 90 > score >= 60:
+            return Difficulty_Level.Easy.value
+        elif 60 > score >= 30:
+            return Difficulty_Level.Hard.value
+        elif 30 > score >= 0:
+            return Difficulty_Level.Very_Hard.value
+
+    if lang == Languages_Used.RUSSIAN:
+        if 100 >= score >= 80:
+            return Difficulty_Level.Very_Easy.value
+        elif 80 > score >= 60:
+            return Difficulty_Level.Easy.value
+        elif 60 > score >= 30:
+            return Difficulty_Level.Hard.value
+        elif 30 > score >= 0:
+            return Difficulty_Level.Very_Hard.value
+
+    if lang == Languages_Used.GERMAN:
+        if 100 >= score >= 80:
+            return Difficulty_Level.Very_Easy.value
+        elif 80 > score >= 60:
+            return Difficulty_Level.Easy.value
+        elif 60 > score >= 40:
+            return Difficulty_Level.Hard.value
+        elif 40 > score >= 0:
+            return Difficulty_Level.Very_Hard.value
+
+    if lang == Languages_Used.FRANCE:
+        if 100 >= score >= 80:
+            return Difficulty_Level.Very_Easy.value
+        elif 80 > score >= 60:
+            return Difficulty_Level.Easy.value
+        elif 60 > score >= 40:
+            return Difficulty_Level.Hard.value
+        elif 40 > score >= 0:
+            return Difficulty_Level.Very_Hard.value
+
+
+
+
+
