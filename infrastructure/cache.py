0273eadeebc6b0ceeea87b36e2743ec113e947f8
valery <iva.sim2007@gmail.com>
2026-09-12 19:18:33 +0700

    clear cache in infrastructure for Nika

--- infrastructure/cache.py
+++
@@ -1,9 +0,0 @@
-from domain.types import AnalysisResult
-
-def get_cached_result(redis_client, text):
-    try:
-        key = text_hash(text)
-        data = redis_client.get(key)
-        return json.loads(data) if data else None
-    except Exception:
-        return None
