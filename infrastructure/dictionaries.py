085da0252cab9d6606f5ac6eb36b09787dffe98e
valery <iva.sim2007@gmail.com>
2026-09-12 21:04:49 +0700

    Add reduced rare word dictionaries

--- infrastructure/dictionaries.py
+++ infrastructure/dictionaries.py
@@ -1,37 +1,20 @@
 import json
 from pathlib import Path
-from ruword_frequency import Frequency
-from wordfreq import get_frequency_dict, zipf_frequency, iter_wordlist
 
-rareDictRu = {}
-BARRIER_RU = 5.0
-freq = Frequency()
-freq.load()
-rare_words_set = set()
-for word in freq.iterate_words(0.0):
-    if freq.ipm(word) < BARRIER_RU:
-        rare_words_set.add(word)
-for word in rare_words_set:
-    rareDictRu[word] = freq.ipm(word)
+DATA_DIR = Path(__file__).parent / 'data'
 
-all_freqs_en = get_frequency_dict('en')
-all_freqs_de = get_frequency_dict('de')
-all_freqs_fr = get_frequency_dict('fr')
-rareDictEn = {}
-rareDictDe = {}
-rareDictFr = {}
+with open(DATA_DIR / 'rareDictEn.json', encoding='utf-8') as f:
+    rareDictEn = json.load(f)
 
-for word, freq in all_freqs_en.items():
-    zipf = zipf_frequency(word, 'en')
-    if zipf < 2.0:
-        rareDictEn[word] = zipf
+with open(DATA_DIR / 'rareDictRu.json', encoding='utf-8') as f:
+    rareDictRu = json.load(f)
 
-for word, freq in all_freqs_de.items():
-    zipf = zipf_frequency(word, 'de')
-    if zipf < 2.0:
-        rareDictDe[word] = zipf
+with open(DATA_DIR / 'rareDictDe.json', encoding='utf-8') as f:
+    rareDictDe = json.load(f)
 
-for word, freq in all_freqs_fr.items():
-    zipf = zipf_frequency(word, 'fr')
-    if zipf < 2.0:
-        rareDictFr[word] = zipf
+with open(DATA_DIR / 'rareDictFr.json', encoding='utf-8') as f:
+    rareDictFr = json.load(f)
+
+
+
+
